343590ade057b5d77d5c7f3112f1c977fe1628ab
teddinata <teddinata@users.noreply.github.com>
2022-07-25 15:44:28 +0700

    Init commit and landing page crowdfund frontend

---
+++ layouts/auth.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="auth-page">
+      <Nuxt />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
